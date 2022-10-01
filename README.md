71ca3d1328e810b6f1a93555a95141135b3bde6d
__coma__ <codeliner96@gmail.com>
2022-10-01 13:45:58 +0900

    Added invalid request process.

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ This is a simple window post message sample application.
 - A new window popup is opened by window A.
 - Enter inputs and submit form.
 - Send response to A window and close this window.
+
+[Sample application page](https://codeliners-post-message-window-a.netlify.app/)
--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from "vue";
+
 // Types
 interface InputForm {
   email: string;
@@ -10,12 +12,14 @@ interface SendMessage {
   command: "open" | "data";
 }
 
+// Variables.
 const allowOrigins = [
-  "http://127.0.0.1:5174",
+  "http://127.0.0.1:5555",
   "https://codeliners-post-message-window-a.netlify.app",
 ];
 let targetOrigin: string | undefined = undefined;
 let sourceOrigin: MessageEventSource | null = null;
+let isPageInvalid = ref(false);
 
 const form = {
   email: "",
@@ -48,7 +52,11 @@ function sendMessage({ data, command }: SendMessage) {
 
 async function onSubmit() {
   // Has source origin?
-  if (!sourceOrigin) return;
+  if (!sourceOrigin) {
+    alert("This page is invalid.\nPlease try again valid way.");
+    isPageInvalid.value = true;
+    return;
+  }
 
   // Send message to opened origin.
   sendMessage({ data: form, command: "data" });
@@ -60,7 +68,8 @@ async function onSubmit() {
 
 <template>
   <div>
-    <form class="form" @submit.prevent="onSubmit">
+    <p v-if="isPageInvalid">This page is invalid.</p>
+    <form v-else class="form" @submit.prevent="onSubmit">
       <input type="text" placeholder="Email" v-model="form.email" />
       <input type="password" placeholder="Password" v-model="form.password" />
       <button type="submit">Submit</button>
